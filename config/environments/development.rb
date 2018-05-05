f33510001c23c0d786bd35f07b2ce93cdd56140c
Daryl Haines <daryl@southeastpixels.com>
2018-05-05 03:32:22 +0000

    Add email with SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://3b69703cef584cb28899bd2b81012890.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Show full error reports.
   config.consider_all_requests_local = true
